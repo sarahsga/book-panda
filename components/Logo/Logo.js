99ab69344107f78bc833de60ccee32fc84b4ef92
Sarah Ahmed <sarahsga4@gmail.com>
2023-03-09 14:38:10 +0500

    run codemod next-image-to-legacy-image

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Logo.module.scss";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 const logo = (props) => (
   <div className={classes.Logo}>
