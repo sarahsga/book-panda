ee32c776b01db89cd685494b46e90465be8c0211
Sarah Ahmed <sarahsga4@gmail.com>
2023-03-20 14:46:24 +0500

    fix: category blocks

--- components/Books/CategoryItem/CategoryItem.js
+++ components/Books/CategoryItem/CategoryItem.js
@@ -7,14 +7,10 @@ const CategoryItem = props => {
   return (
     <article className={classes.CategoryItem} onClick={props.clicked}>
       <span className={classes.image}>
-        <Image
+        <img
           src="/images/bg-book-category-tile.jpg"
           alt={props.category.label}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: "contain"
-          }} />
+           />
       </span>
       <Link href="/">
         <h2>{props.category.label}</h2>
